fe972303da65d89eb0bd27ea70785d42bdce9960
tomas Salas <tomassubero97@gmail.com>
2021-04-03 13:31:39 -0500

    last revized added error handler and image fix

--- routes/project.js
+++ routes/project.js
@@ -3,8 +3,15 @@ const data = require("../data.json");
 const projects = data.projects;
 const router = express.Router();
 
-router.get("/:id", (req, res) => {
-    res.render("../views/project", { project: projects[req.params.id] }) });
-
+router.get("/:id", (req, res, next) => {
+    //res.render("../views/project", { project: projects[req.params.id] }) });
+    if (projects[req.params.id]) {
+        res.render("../views/project", { project: projects[req.params.id] });
+    } else {
+        const err = new Error('Oops! this page does not exist');
+        err.status = 404;
+        next(err);
+    }
+});
 
 module.exports = router;
